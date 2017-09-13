b499731c62480feb021f1c3a761828fed303d51b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-09-13 13:29:17 +0200

    Implement function to find linebreaks in document

--- src/parsemon/sourcemap.py
+++ src/parsemon/sourcemap.py
@@ -1,5 +1,15 @@
+from typing import List
+
+
 def display_location(line, column):
     return 'L: {line}, C: {column}'.format(
         line=line,
         column=column,
     )
+
+def find_linebreak_indices(document) -> List[int]:
+    def iterate():
+        for (character, index) in zip(document,range(0,len(document))):
+            if character == '\n':
+                yield index
+    return list(iterate())
--- tests/test_sourcemap.py
+++ tests/test_sourcemap.py
@@ -1,4 +1,4 @@
-from parsemon.sourcemap import display_location
+from parsemon.sourcemap import display_location, find_linebreak_indices
 
 
 def test_display_location_contains_the_actual_line_number():
@@ -8,3 +8,11 @@ def test_display_location_contains_the_actual_line_number():
 def test_dist_location_contains_the_actual_column_number_supplied():
     column = 465
     assert str(column) in display_location(line=0, column=column)
+
+def test_find_linebreak_indices_returns_empty_list_for_online_string():
+    msg = "12345"
+    assert find_linebreak_indices(msg) == []
+
+def test_find_linebreak_indices_returns_list_as_long_as_msg_when_only_linebreaks():
+    msg = '\n\n\n\n'
+    assert len(find_linebreak_indices(msg)) == len(msg)
