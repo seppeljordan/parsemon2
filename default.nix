cfeb77f2a4f47d84f83053cccfc1f51698e32895
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-09-13 15:36:32 +0200

    Fix default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,21 @@
 with (import <nixpkgs> {});
 
-(callPackage ./default.nix {
+let
+  f =
+    { buildPythonPackage, pytest, mypy, flake8, lib }:
+    buildPythonPackage {
+      name = "parsemon2";
+      buildInputs = [ pytest mypy flake8 ];
+      src = lib.cleanSource ./.;
+      checkPhase = ''
+        sh run-tests.sh
+      '';
+      shellHook = ''
+        export PYTHONPATH=$PWD/src:$PYTHONPATH
+      '';
+    };
+in
+(callPackage f {
   buildPythonPackage = python3Packages.buildPythonPackage;
   pytest = python3Packages.pytest;
   flake8 = python3Packages.flake8;
