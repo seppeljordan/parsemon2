f380f2b4f97666fdd07cdedb715e4fabd1d2cb76
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2017-09-13 15:38:12 +0200

    PEP8

--- src/parsemon/sourcemap.py
+++ src/parsemon/sourcemap.py
@@ -7,12 +7,14 @@ def display_location(line, column):
         column=column,
     )
 
+
 def find_linebreak_indices(document) -> List[int]:
     def iterate():
-        for (character, index) in zip(document,range(0,len(document))):
+        for (character, index) in zip(document, range(0, len(document))):
             if character == '\n':
                 yield index
     return list(iterate())
+
 
 def find_line_in_indices(location, indices):
     if not indices:
