1c5afb8056fda9159dfdc0e4e18f10e97bf6408b
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2018-02-28 13:23:03 +0100

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from distutils.core import setup
 
 setup(
     name='parsemon2',
+    description=u"A monadic parser combinator written purely in python",
     package_dir = {'': 'src'},
+    version="1.0",
+    license="GPL-3",
+    packages=['parsemon']
 )
