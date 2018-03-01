3d8505f7bc32a8f22c35615cde57c42fe2c5e096
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2018-03-01 16:45:29 +0100

    Add sphinx setup

--- default.nix
+++ default.nix
@@ -2,10 +2,10 @@ with (import <nixpkgs> {});
 
 let
   f =
-    { buildPythonPackage, pytest, mypy, flake8, lib }:
+    { buildPythonPackage, pytest, mypy, flake8, sphinx, lib }:
     buildPythonPackage {
       name = "parsemon2";
-      buildInputs = [ pytest mypy flake8 ];
+      buildInputs = [ pytest mypy flake8 sphinx];
       src = lib.cleanSource ./.;
       checkPhase = ''
         sh run-tests.sh
@@ -13,10 +13,20 @@ let
       shellHook = ''
         export PYTHONPATH=$PWD/src:$PYTHONPATH
       '';
+      postPhases = [ "buildDocsPhase" "installDocsPhase" ];
+      buildDocsPhase = ''
+        make man
+      '';
+      installDocsPhase = ''
+        mkdir -p $out/share/man/man3
+        cp build/man/parsemon2.3 $out/share/man/man3/parsemon2.3
+      '';
     };
+  drv = callPackage f {
+    buildPythonPackage = python3Packages.buildPythonPackage;
+    pytest = python3Packages.pytest;
+    flake8 = python3Packages.flake8;
+    sphinx = python3Packages.sphinx;
+  };
 in
-(callPackage f {
-  buildPythonPackage = python3Packages.buildPythonPackage;
-  pytest = python3Packages.pytest;
-  flake8 = python3Packages.flake8;
-})
+drv
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. parsemon2 documentation master file, created by
+   sphinx-quickstart on Thu Mar  1 16:20:54 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to parsemon2's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: parsemon
+   :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
