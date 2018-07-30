220bfbf1f5f52707da80ab8a8b197c79606f5931
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2018-07-30 21:36:12 +0200

    Implement Validator in terms of trampolines to enable TCO

--- src/parsemon/validator.py
+++ src/parsemon/validator.py
@@ -1,6 +1,7 @@
 from typing import Callable, Generic, Sized, Tuple, TypeVar
 
 from .parser import Parser, bind, fail, unit
+from .trampoline import Call, Result, Trampoline, with_trampoline
 
 T = TypeVar('T')
 
@@ -8,7 +9,7 @@ T = TypeVar('T')
 class Validator(Generic[T]):
     def __init__(
             self,
-            function: Callable[[T], Tuple[bool, str]]
+            function: Callable[[T], Trampoline[Tuple[bool, str]]]
     ) -> None:
         self.function = function
 
@@ -20,7 +21,11 @@ class Validator(Generic[T]):
         def do_validation(
                 to_validate: T,
         ) -> Parser[T, Sized]:
-            is_validated, error_message = self.function(to_validate)
+            is_validated, error_message = with_trampoline(
+                self.function
+            )(
+                to_validate
+            )
             if is_validated:
                 return unit(to_validate)
             else:
@@ -33,44 +38,56 @@ class Validator(Generic[T]):
 
     def __or__(self, other):
         def validator_function(value):
-            self_result, self_error_msg = self.function(value)
+            self_result, self_error_msg = with_trampoline(
+                self.function
+            )(
+                value
+            )
             if self_result:
-                return (self_result, self_error_msg)
+                return Result((self_result, self_error_msg))
             else:
-                return other.function(value)
+                return Call(other.function, value)
         return Validator(validator_function)
 
     def __and__(self, other):
         def validator_function(value):
-            self_result, self_error = self.function(value)
-            other_result, other_error = other.function(value)
-            return (
+            self_result, self_error = with_trampoline(
+                self.function
+            )(
+                value
+            )
+            other_result, other_error = with_trampoline(
+                other.function
+            )(
+                value
+            )
+            return Result((
                 self_result and other_result,
                 "{self_error} OR {other_error}".format(
                     self_error=self_error,
                     other_error=other_error,
-                )
+                ))
             )
         return Validator(validator_function)
 
 
 even: Validator[int]
 even = Validator(
-    lambda n: (
+    lambda n: Result((
         n % 2 == 0,
         'Expect even number but got {n}'.format(
             n=n
         )
-    )
+    ))
 )
 
 
 odd: Validator[int]
 odd = Validator(
-    lambda n: (
+    lambda n: Result((
         n % 2 == 1,
         'Expected odd number but got {n}'.format(
             n=n
         )
-    )
+    ))
 )
