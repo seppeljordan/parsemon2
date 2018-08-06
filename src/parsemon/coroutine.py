09572008d0c7daf268ce4295cb7a60363ea3c67e
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2018-08-06 02:34:23 +0200

    Add docstring to do function

--- src/parsemon/coroutine.py
+++ src/parsemon/coroutine.py
@@ -3,6 +3,24 @@ from .parser import unit
 
 
 def do(f):
+    """Use this as a decorator
+
+    Do allows you to combine parsers in a more pythonic way.
+
+    It expects a generator that returns something in the end and takes
+    no arguments.
+
+    It lets you write parsers like the following:
+
+    .. code:: python
+      @do
+      def 3_letters():
+          first = yield character()
+          second = yield character()
+          third = yield character()
+          return first + second + third
+
+    """
     class StartIteration:
         pass
 
