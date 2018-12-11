03f1b9032f8b33b199bbaa787270fb7de2f84a3e
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2018-12-11 18:59:42 +0100

    Implement performancetest to measure run time behavior of parsers with out any choices

---
+++ tests/test_performance_without_choice.py
@@ -0,0 +1,41 @@
+import pytest
+
+from parsemon import do, literal, run_parser, chain, unit
+
+
+@pytest.fixture(
+    params=[
+        1,2,3,4,5,10,20
+    ]
+)
+def document(request):
+    document_length = request.param
+    return '0123456789' * 1000 * document_length
+
+
+@pytest.fixture(
+    params=(
+        'do', 'chain'
+    )
+)
+def implementation(request):
+    if request.param == 'do':
+        def wrapper(document):
+            @do
+            def parser():
+                for letter in document:
+                    yield literal(letter)
+                return True
+            return parser()
+    else:
+        def wrapper(document):
+            parser = unit(True)
+            for character in document:
+                parser = chain(parser, literal(character))
+            return parser
+    return wrapper
+
+
+def test_performance_without_choice(document, benchmark, implementation):
+    parser = implementation(document)
+    assert benchmark(run_parser, parser, document)
