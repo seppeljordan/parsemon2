fefd4e25d7c71f5c69972d35999cd9135cb57e5f
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-12-21 12:57:03 +0100

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -8,26 +8,19 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     let
-      packageOverrides = import nix/package-overrides.nix;
-      overlay = final: prev: {
-        python3 = prev.python3.override { inherit packageOverrides; };
-        python3Packages = final.python3.pkgs;
-        python36 = prev.python36.override { inherit packageOverrides; };
-        python36Packages = final.python36.pkgs;
-        python37 = prev.python37.override { inherit packageOverrides; };
-        python37Packages = final.python37.pkgs;
-        python38 = prev.python38.override { inherit packageOverrides; };
-        python38Packages = final.python38.pkgs;
-        python39 = prev.python39.override { inherit packageOverrides; };
-        python39Packages = final.python39.pkgs;
-      };
       systemDependent = flake-utils.lib.eachDefaultSystem (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ overlay ];
+            overlays = [ self.overlay ];
           };
           python = pkgs.python3;
+          runCodeAnalysis = name: command:
+            pkgs.runCommand "${name}-parsemon2" { } ''
+              cd ${self}    
+              ${command}
+              mkdir $out
+            '';
         in {
           devShell = pkgs.mkShell {
             buildInputs = [
@@ -59,43 +52,50 @@
             python37 = pkgs.python37.pkgs.parsemon2;
             python38 = pkgs.python38.pkgs.parsemon2;
             python39 = pkgs.python39.pkgs.parsemon2;
-            nixfmt-check = pkgs.runCommand "nixfmt-parsemon2" { } ''
+            nixfmt-check = runCodeAnalysis "nixfmt" ''
               ${pkgs.nixfmt}/bin/nixfmt --check \
-                $(find ${self} -type f -name '*.nix')
-              mkdir $out
+                  $(find ${self} -type f -name '*.nix')
             '';
-            black-check = pkgs.runCommand "black-parsemon2" { } ''
-              cd ${self}
+            black-check = runCodeAnalysis "black" ''
               ${python.pkgs.black}/bin/black --check .
-              mkdir $out
             '';
-            mypy-check = pkgs.runCommand "mypy-parsemon2" { } ''
-              cd ${self}
+            mypy-check = runCodeAnalysis "mypy" ''
               ${python.pkgs.mypy}/bin/mypy parsemon
-              mkdir $out
             '';
-            isort-check = pkgs.runCommand "isort-parsemon2" { } ''
-              cd ${self}
+            isort-check = runCodeAnalysis "isort" ''
               ${python.pkgs.isort}/bin/isort \
                   --settings-path setup.cfg \
                   --check-only \
                   -df \
                   . \
                   test-pypi-install
-              mkdir $out
             '';
-            flake8-check = pkgs.runCommand "flake8-parsemon2" { } ''
-              cd ${self}
+            flake8-check = runCodeAnalysis "flake8" ''
               ${python.pkgs.flake8}/bin/flake8
-              mkdir $out
             '';
-            nix-linter-check = pkgs.runCommand "nix-linter-parsemon2" { } ''
-              cd ${self}
+            nix-linter-check = runCodeAnalysis "nix-linter" ''
               ${pkgs.nix-linter}/bin/nix-linter -r .
-              mkdir $out
             '';
           };
         });
-      systemIndependent = { inherit overlay; };
+      systemIndependent = {
+        lib = {
+          packageOverrides = import nix/package-overrides.nix;
+          package = import nix/parsemon2.nix;
+        };
+        overlay = final: prev:
+          with self.lib; {
+            python3 = prev.python3.override { inherit packageOverrides; };
+            python3Packages = final.python3.pkgs;
+            python36 = prev.python36.override { inherit packageOverrides; };
+            python36Packages = final.python36.pkgs;
+            python37 = prev.python37.override { inherit packageOverrides; };
+            python37Packages = final.python37.pkgs;
+            python38 = prev.python38.override { inherit packageOverrides; };
+            python38Packages = final.python38.pkgs;
+            python39 = prev.python39.override { inherit packageOverrides; };
+            python39Packages = final.python39.pkgs;
+          };
+      };
     in systemDependent // systemIndependent;
 }
