89ce4d31b68b1896e2f61e9fece03917ce7c7c9a
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-12-27 21:44:41 +0100

    parsemon2.nix: Remove unused parameter

--- nix/parsemon2.nix
+++ nix/parsemon2.nix
@@ -1,8 +1,24 @@
 { buildPythonPackage, attrs, hypothesis, pytest, pytest-benchmark
-, pytest-profiling, pytestcov, sphinx, setuptools-rust, cargo, rustc, python }:
+, pytest-profiling, pytestcov, sphinx, setuptools-rust, cargo, rustc
+, pytestCheckHook }:
 buildPythonPackage {
   pname = "parsemon2";
   version = "dev";
+  nativeBuildInputs = [ sphinx rustc cargo ];
+  propagatedBuildInputs = [ attrs cargo rustc ];
+  buildInputs = [ setuptools-rust pytestCheckHook ];
+  src = ../.;
+  postPhases = [ "buildDocsPhase" "installDocsPhase" ];
+  buildDocsPhase = ''
+    make man 
+  '';
+  installDocsPhase = ''
+    mkdir -p $out/share/man/man3
+    cp build/man/parsemon2.3 $out/share/man/man3/parsemon2.3
+  '';
+
+  # tests
+  pytestFlagsArray = [ "--benchmark-skip" ];
   checkInputs = [
     hypothesis
     pytest
@@ -11,20 +27,4 @@ buildPythonPackage {
     pytestcov
     setuptools-rust
   ];
-  nativeBuildInputs = [ sphinx rustc cargo ];
-  propagatedBuildInputs = [ attrs cargo rustc ];
-  buildInputs = [ setuptools-rust ];
-  # buildPhase = ''
-  #   ${python.interpreter} setup.py bdist_wheel
-  # '';
-  src = ../.;
-  postPhases = [ "buildDocsPhase" "installDocsPhase" ];
-  buildDocsPhase = ''
-    make man
-  '';
-  doCheck = false;
-  installDocsPhase = ''
-    mkdir -p $out/share/man/man3
-    cp build/man/parsemon2.3 $out/share/man/man3/parsemon2.3
-  '';
 }
