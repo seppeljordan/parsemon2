a06df3c5292e63d26a55b5a10e7373e556dfe7a1
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-01-23 12:01:39 +0100

    Don't use custom version of setuptools-rust

--- nix/package-overrides.nix
+++ nix/package-overrides.nix
@@ -4,12 +4,4 @@ self: super: {
   pytest-profiling = self.callPackage ./pytest-profiling.nix { };
   bumpv = self.callPackage ./bumpv.nix { };
   parsemon2 = self.callPackage ./parsemon2.nix { };
-  setuptools-rust = super.setuptools-rust.overridePythonAttrs (_old: {
-    src = fetchFromGitHub {
-      owner = "seppeljordan";
-      repo = "setuptools-rust";
-      rev = "12c96f0539189d560e09f6338ea1c1a23ee9ff3e";
-      sha256 = "g7r7KamiYyagx7kvLW9Iwuv5MSAAm7w964RXXXtGJCc=";
-    };
-  });
 }
