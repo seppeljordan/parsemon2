1b2a2d8b1a4285be547624d670634e8fce0904f9
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-02-12 15:33:45 +0100

    Remove line about non existing rust extension from setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,6 @@ setup(
     rust_extensions=[
         RustExtension(
             "parsemon.result", "Cargo.toml", debug=False, args=cargo_arguments
-        ),
-        RustExtension(
-            "parsemon.internals.parsers",
-            "Cargo.toml",
-            debug=False,
-            args=cargo_arguments,
-        ),
+        )
     ]
 )
