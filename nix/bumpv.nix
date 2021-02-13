851f031bc349d8e9c89911260772517f476d2174
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-02-14 00:13:33 +0100

    Remove bumpv package from nix code

--- nix/bumpv.nix
+++
@@ -1,8 +0,0 @@
-{ buildPythonPackage, fetchFromGitHub, pyaml, click }:
-
-buildPythonPackage rec {
-  pname = "bumpv";
-  version = "0.3.0";
-  src = with builtins; fetchFromGitHub (fromJSON (readFile ./bumpv.json));
-  propagatedBuildInputs = [ pyaml click ];
-}
