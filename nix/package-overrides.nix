065f6835fa09476243ffd3481b468590155e1f63
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-08-14 19:48:50 +0200

    Remove obsolete package definition of gprof2dot

--- nix/package-overrides.nix
+++ nix/package-overrides.nix
@@ -1,5 +1,4 @@
 self: super: {
-  gprof2dot = self.callPackage ./gprof2dot.nix { };
   pytest-profiling = self.callPackage ./pytest-profiling.nix { };
   parsemon2 = self.callPackage ./parsemon2.nix { };
 }
