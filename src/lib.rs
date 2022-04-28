c899ea055023748d37ce25541cfbe28c355a76c8
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2022-04-28 22:48:55 +0200

    Fix rust compiler warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
-use pyo3::class::number::PyNumberProtocol;
-use pyo3::gc::{PyGCProtocol, PyVisit};
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
-use pyo3::PyTraverseError;
 
 mod primitives;
 mod result;
-mod stream;
 mod trampoline;
 
 fn add_submodule<F>(
@@ -29,6 +24,5 @@ fn extensions(py: Python<'_>, module: &PyModule) -> PyResult<()> {
     add_submodule(py, "result", result::initialize_module, &module)?;
     add_submodule(py, "trampoline", trampoline::initialize_trampoline, &module)?;
     add_submodule(py, "primitives", primitives::initialize_module, &module)?;
-    add_submodule(py, "stream", stream::initialize_module, &module)?;
     Ok(())
 }
